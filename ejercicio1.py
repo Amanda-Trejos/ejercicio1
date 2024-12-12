2ae6706be24c6fa3f4c56ada101be2a1d29e7f61
Amanda <amandatrejos797@gmail.com>
2024-12-12 00:32:29 -0600

    Modificando valores

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,3 @@
-import os
-
 libros = {
     "1984": {"autor": "George Orwell", "año": 1949, "género": "Distopía", "calificación": 4.8},
     "To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género": "Ficción", "calificación": 4.9},
@@ -12,3 +10,6 @@ print(f"Autor: {libros[libro]['autor']}")
 print(f"Año de publicación: {libros[libro]['año']}")
 print(f"Género: {libros[libro]['género']}")
 print(f"Calificación: {libros[libro]['calificación']}")
+
+libros["1984"]["calificación"] = 5.0
+print(f"Nueva calificación de '1984': {libros['1984']['calificación']}")
